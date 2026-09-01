71e4f32ef5f30a65ff37fabacbc0830f9fe50c0e
immerse09 <nayakaman7172@gmail.com>
2026-09-01 18:30:06 +0000

    done

---
+++ Recursion/sumOf_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int print(int n){
+    if(n==1){
+        return 1;
+    }
+
+    return n + print(n-1);
+}
+
+int main(){
+   
+    cout << print(10) << endl;
+}
